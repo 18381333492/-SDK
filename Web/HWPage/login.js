40914c17a63805e2234c9143dbed2a17bd60a129
Administrator <Administrator@MS-20170511IAEE>
2017-10-20 17:16:36 +0800

    123546

---
+++ Web/HWPage/login.js
@@ -0,0 +1,27 @@
+﻿
+
+//获取账户信息
+function hw_getInfo() {
+    try {
+        var result = window.lives.getAccount(); //获取账户信息
+        if (result != 3) {
+            result = JSON.parse(result);
+            return result.account; //返回登录的账号
+        }
+        else {
+            return null;
+        }
+    } catch (e) {
+        alert(e.message);
+    }
+}
+
+
+function setAccount(response) {
+    var result = JSON.parse(response);
+    if (result.code == 200) {
+        //登录成功
+        var account = result.account;
+        alert("登录成功");
+    }
+}
---
+++ Web/login.js
@@ -0,0 +1,63 @@
+﻿
+
+
+//获取账户信息
+function hw_getInfo() {
+    var  account = localStorage.getItem("accountInfo");
+    if (account) {
+        return account;
+    }
+    else {
+        var result = window.lives.getAccount();//获取账户信息
+        if (result != "") {
+            result = JSON.parse(result);
+            localStorage.setItem("accountInfo", result.account);
+            return result.account;//返回登录的账号
+        }
+        else {
+            return "";
+        }
+    }
+}
+
+location.href="hwpayment.html";
+
+//登录成功 华为异步调用
+function setAccount(response) {
+    var result = JSON.parse(response);
+    if (result.code==200) {
+        //登录成功
+        var account = result.account;
+        localStorage.setItem("accountInfo", account);
+    }
+    else {
+        //登录失败
+        alert(result.code+"  "+result.message);
+    }
+}
+
+try{
+    var account = localStorage.getItem("accountInfo");
+    alert(account)
+    if (account) {
+        alert(-1);
+        return account;
+    }
+    else {
+        alert(0);
+        alert(window.hw_lives);
+        alert(window.lives);
+        var result = window.hw_lives.getAccount(); //获取账户信息
+        if (result != "") {
+            result = JSON.parse(result);
+            localStorage.setItem("accountInfo", result.account);
+            return result.account; //返回登录的账号
+        }
+        else {
+            return "";
+        }
+    }
+}
+catch(e){
+    alert(e.Message)
+}
